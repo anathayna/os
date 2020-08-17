e88eac13c0e5ac47e855eebf39c4902f5ad25fc5
anathayna <anathaynafranca@gmail.com>
2020-08-17 04:38:42 -0300

    starting the list

--- 01/main.c
+++ 01/main.c
@@ -2,7 +2,50 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-  printf("Hello, World!\n");
+typedef struct stringList {
+  char string;
+  struct stringList *next;
+}sList;
+
+typedef struct nameList {
+  sList *start;
+  sList *end;
+}nList;
+
+void init(nList *p){
+  p->start = NULL;
+  p->end = NULL;  
+}
+
+void insert(nList *p){
+  sList *no;
+  no = malloc (sizeof (char));
+  scanf("%c",&no->string);
+  no->next = NULL;
+
+  if (p->start == NULL){
+    p->start = no;
+    p->end = no;  
+  } else {
+    p->end->next = no;
+    p->end = no; 
+  }
+}
+
+void reverseList(sList *e, sList *ant){
+  if(e->next != NULL)
+    reverseList(e->next, e);
+  e->next = ant;
+}
+
+void reverse(nList *p){
+  reverseList(p->start, NULL);
+
+  sList *aux = p->start;
+  p->start = p->end;    
+  p->end = aux;
+}
+
+int main(void) {
   return 0;
 }
