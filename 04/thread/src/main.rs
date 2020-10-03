d703fe770994476dd18675b558f45babad24b29d
anathayna <anathaynafranca@gmail.com>
2020-10-02 22:34:27 -0300

    add second thread

--- 04/thread/src/main.rs
+++ 04/thread/src/main.rs
@@ -2,14 +2,23 @@ use std::{thread, time};
 
 fn main() {
     let start = time::Instant::now();
-    let pause = time::Duration::from_millis(500);
+    let mut pause = time::Duration::from_millis(5000);
 
-    let handler = thread::spawn( move || {
+    let handler1 = thread::spawn( move || {
         thread::sleep(pause.clone());
-        println!("fim thread");
+        println!("fim thread 1");
+    });
+
+    pause = time::Duration::from_millis(500);
+
+    let handler2 = thread::spawn( move || {
+        thread::sleep(pause.clone());
+        println!("fim thread 2");
     });
     
-    handler.join().unwrap();
+    handler1.join().unwrap();
+    //handler2.join().unwrap();
+
     let finish = time::Instant::now();
     println!("{:02?}", finish.duration_since(start));
 }
