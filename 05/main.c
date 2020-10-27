1aebaf6817bd9685bf216cffa5a7674f44728327
ana thayna <anathaynafranca@gmail.com>
2020-10-27 17:30:10 -0300

    restart word count exercice

--- 05/main.c
+++ 05/main.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 
+void *alfa(void *);
+void count(int fcount, char *fname[]);
+
 int main(int argc, char *argv[]) {
-  return 0;
+  if (argc > 1) {
+    count(argc - 1, argv);
+  }
+  exit(EXIT_SUCCESS);
 }
 
-//gcc main.c
-//./a.out
+void *alfa(void *fn) {
+  const char *fname = (char *) fn;
+  FILE *f;
+
+  f = fopen(fname, "r");
+  if (f != NULL) {
+    int fsize = sizeof(f);
+    char *mem = malloc(fsize);
+    int word = 0;
+
+    while (fscanf(f, "%s", mem) == 1) {
+      word++;
+    }
+    free(mem);
+
+    printf("%s: %d words\n", fname, word);
+  } else {
+    printf("file %s not found.\n", fname);
+  }
+
+  fclose(f);
+  return NULL;
+}
+
+
+void count(int fcount, char *fname[]) {
+  int nthreads = fcount;
+  pthread_t t[nthreads]; 
+
+  for (int i = 1; i <= fcount; i++) {
+    pthread_create(&t[i - 1], NULL, alfa, fname[i]);
+  }
+
+  for(int i = 0; i < nthreads; i++) {
+    pthread_join(t[i], NULL);
+  }
+}
+
