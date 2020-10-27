f962e582830d905db837ae2a04ed7ee10143fba5
ana thayna <anathaynafranca@gmail.com>
2020-10-27 10:59:32 -0300

    continue valgrind

--- 04/valgrind/main.c
+++ 04/valgrind/main.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 
 //protótipo
+void trace(const char *);
 void func(void);
 
 int main(void) {
+    trace(__FUNCTION__);
     func();
     exit(EXIT_SUCCESS);
 }
 
-void func(void){
-    printf("trace func");
+void func(void) {
+    trace(__FUNCTION__);
+}
+
+void trace(const char *f){
+    printf("estou na função nomeada '%s'\n", f);
 }
