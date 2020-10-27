4ebe26dd114733f9cf38f204b958729c67e7c2c1
ana thayna <anathaynafranca@gmail.com>
2020-10-27 16:35:34 -0300

    finish thread count

--- 04/valgrind/main.c
+++ 04/valgrind/main.c
@@ -6,20 +6,27 @@
 
 #define N_THREADS 15
 
+void alfa(void);
 void *imprimir_msg(void *msg);
 
 int main(void) {
+    alfa();
+    sleep(3);
+    exit(EXIT_SUCCESS);
+}
+
+void alfa(void) {
     pthread_t th[N_THREADS];
+    int thn[N_THREADS];
 
     for(int i=0; i<N_THREADS; i++) {
-        pthread_create(&th[i], NULL, imprimir_msg, &i);
+        thn[i] = i;
+        pthread_create(&th[i], NULL, imprimir_msg, &thn[i]);
     }
 
     for(int i=0; i<N_THREADS; i++) {
         pthread_join(th[i], NULL);
     }
-    
-    exit(EXIT_SUCCESS);
 }
 
 void *imprimir_msg(void *m) {
