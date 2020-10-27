2fb08ffb2f9b3ebfef88bdb6d951253a226e2403
ana thayna <anathaynafranca@gmail.com>
2020-10-27 11:30:09 -0300

    finish 2nd thread example

--- 04/valgrind/main.s
+++ 04/valgrind/main.s
@@ -1,81 +1,66 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 10, 15	sdk_version 10, 15, 6
-	.intel_syntax noprefix
 	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
 	.cfi_startproc
 ## %bb.0:
-	push	rbp
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset rbp, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register rbp
-	sub	rsp, 16
-	mov	dword ptr [rbp - 4], 0
-	lea	rdi, [rip + L___FUNCTION__.main]
-	call	_trace
-	call	_func
-	xor	edi, edi
-	call	_exit
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$112, %rsp
+	leaq	-96(%rbp), %rdi
+	movq	___stack_chk_guard@GOTPCREL(%rip), %rax
+	movq	(%rax), %rax
+	movq	%rax, -8(%rbp)
+	leaq	L_.str(%rip), %rsi
+	movl	$80, %edx
+	callq	___strcpy_chk
+	leaq	-96(%rbp), %rdi
+	movq	%rax, -104(%rbp)        ## 8-byte Spill
+	callq	_mudar
+	leaq	-96(%rbp), %rdi
+	callq	_puts
+	movq	___stack_chk_guard@GOTPCREL(%rip), %rcx
+	movq	(%rcx), %rcx
+	movq	-8(%rbp), %rdx
+	cmpq	%rdx, %rcx
+	jne	LBB0_2
+## %bb.1:
+	xorl	%eax, %eax
+	addq	$112, %rsp
+	popq	%rbp
+	retq
+LBB0_2:
+	callq	___stack_chk_fail
+	ud2
 	.cfi_endproc
                                         ## -- End function
-	.globl	_trace                  ## -- Begin function trace
+	.globl	_mudar                  ## -- Begin function mudar
 	.p2align	4, 0x90
-_trace:                                 ## @trace
+_mudar:                                 ## @mudar
 	.cfi_startproc
 ## %bb.0:
-	push	rbp
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset rbp, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register rbp
-	sub	rsp, 16
-	mov	qword ptr [rbp - 8], rdi
-	mov	rsi, qword ptr [rbp - 8]
-	lea	rdi, [rip + L_.str.1]
-	mov	al, 0
-	call	_printf
-	add	rsp, 16
-	pop	rbp
-	ret
-	.cfi_endproc
-                                        ## -- End function
-	.globl	_func                   ## -- Begin function func
-	.p2align	4, 0x90
-_func:                                  ## @func
-	.cfi_startproc
-## %bb.0:
-	push	rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset rbp, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register rbp
-	sub	rsp, 16
-	lea	rdi, [rip + L___FUNCTION__.func]
-	call	_trace
-	lea	rax, [rip + L_.str]
-	mov	qword ptr [rbp - 8], rax
-	mov	rax, qword ptr [rbp - 8]
-	mov	byte ptr [rax], 97
-	mov	rdi, qword ptr [rbp - 8]
-	call	_puts
-	add	rsp, 16
-	pop	rbp
-	ret
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movq	%rdi, -8(%rbp)
+	cmpq	$0, -8(%rbp)
+	je	LBB1_2
+## %bb.1:
+	movq	-8(%rbp), %rax
+	movb	$35, (%rax)
+LBB1_2:
+	popq	%rbp
+	retq
 	.cfi_endproc
                                         ## -- End function
 	.section	__TEXT,__cstring,cstring_literals
-L___FUNCTION__.main:                    ## @__FUNCTION__.main
-	.asciz	"main"
-
-L___FUNCTION__.func:                    ## @__FUNCTION__.func
-	.asciz	"func"
-
 L_.str:                                 ## @.str
-	.asciz	"ana thayna"
-
-L_.str.1:                               ## @.str.1
-	.asciz	"estou na fun\303\247\303\243o nomeada '%s'\n"
+	.asciz	"  ana thayna"
 
 .subsections_via_symbols
