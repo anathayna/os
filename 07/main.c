8f19091110110da6585bcd8bd29db08d30594703
ana thayna <anathaynafranca@gmail.com>
2020-11-20 15:41:26 -0300

    asterisk

--- 07/main.c
+++ 07/main.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main() {
-    while(1)
-        printf("*\n");
+#define BUFFER_SIZE 128
+
+void initBuffer(char *buff);
+void off(void);
+
+// int main() {
+//     while(1)
+//         printf("*\n");
+// }
+
+int main(void) {
+    unsigned int eax=11,ebx=0,ecx=1,edx=0;
+    asm volatile("cpuid"
+            : "=a" (eax),
+              "=b" (ebx),
+              "=c" (ecx),
+              "=d" (edx)
+            : "0"  (eax), 
+              "2"  (ecx)
+            : );
+
+    int n = eax;
+    int p = n - 1; 
+
+    for (int i=0;i<p;i++) {
+        fork();
+    }
+
+    off();
+
+    return(EXIT_SUCCESS);
+}
+
+void initBuffer(char *buff) {
+    for (int i=0;i<BUFFER_SIZE;) {
+        buff[i] = '*';
+        buff[i+1] = '\n';
+        i += 2;
+    }
+    buff[BUFFER_SIZE - 1] = '\0';
+}
+
+void off(void) {
+    char msg[BUFFER_SIZE];
+    initBuffer(msg);
+    start:
+        puts(msg);
+        goto start;
 }
