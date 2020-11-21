8c7339b7eaef0c4261c34e6c4e5a38e1c11e9a43
ana thayna <anathaynafranca@gmail.com>
2020-11-21 11:45:10 -0300

    finish

--- 08/main.c
+++ 08/main.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-struct point {
+char memory[2048];
+
+struct mem {
   size_t size;
   int free; 
-  struct point *next; 
+  struct mem *next; 
 };
+
+struct mem *m = (void *)memory;
 
 void *alloc(size_t nbytes);
 void free(void *p);
 void init();
-
-char memory[2048];
-struct point *m = (void *)memory;
 
 int main(void) {
     init();
@@ -21,19 +22,19 @@ int main(void) {
     *p = 100;
 
     printf("[1] p = %d\n", *p);
-    printf("[1] p memory: %d\n", p);
+    printf("[2] p memory: %d\n", p);
     
     free(p);
 }
 
 void init() {
-    m->size = memory - sizeof(struct point);
+    m->size = sizeof(memory) - sizeof(struct mem);
     m->free = 1;
     m->next = NULL;
 }
 
 void *alloc(size_t nbytes) {
-    struct point *this, *after;
+    struct mem *this, *after;
     void *resul;
     this = m;
 
@@ -43,6 +44,8 @@ void *alloc(size_t nbytes) {
         after = this;
         this = this->next;
     }
+
+    printf("[3] memory: %d\n", this);
 
     if(this->size > nbytes || this->size == nbytes) {
         this->free = 0;
@@ -57,7 +60,12 @@ void *alloc(size_t nbytes) {
 }
 
 void free(void *p) {
-    if (p != NULL) {
-        free(p);
+    if(p >= (void *)memory && p <= (void *)(memory + sizeof(memory))) {
+        struct mem *this = p;
+        this--;
+        this->free = 1;
+    } else {
+        exit(EXIT_FAILURE);
     }
+    printf("[4] free: %d\n", free);
 }
